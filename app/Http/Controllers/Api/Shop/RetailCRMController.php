380f8c563e52c18a428d26f834f61c934a628e53
Yuri Bondar <trigur@yandex.ru>
2018-11-12 11:40:01 +0300

    fix retail discharge

--- app/Http/Controllers/Api/Shop/RetailCRMController.php
+++ app/Http/Controllers/Api/Shop/RetailCRMController.php
@@ -14,23 +14,21 @@ class RetailCRMController extends Controller
     public function catalog()
     {
         $categories = Categories::getCollection();
-
         $suppliers = Supplier::all();
 
         $products = Product::with([
             'currentPrice',
-            'categories',
+            'categoryRelations',
             'attributes',
             'attributeOptions',
             'image'
         ])
             ->get();
 
-
         return response()->view('shop.api.catalog-xml', [
-            'categories' => json_decode($categories),
-            'products' => json_decode($products),
-            'suppliers' => json_decode($suppliers),
+            'categories' => $categories,
+            'products' => $products,
+            'suppliers' => $suppliers
         ])
             ->header('Content-Type', 'text/xml');
 
--- resources/views/shop/api/catalog-xml.blade.php
+++ resources/views/shop/api/catalog-xml.blade.php
@@ -5,37 +5,40 @@
         <company>ООО "МОССЭБО КМ"</company>
         <categories>
             @foreach($categories as $category)
-                <category id="{{ $category->id }}"
-                          @if(!is_null($category->parent_id)) parentId="{{ $category->parent_id }}" @endif >
-                    {{{ $category->current_i18n->title }}}
-                </category>
+                <category id="{{ $category->id }}" @if(!is_null($category->parent_id)) parentId="{{ $category->parent_id }}" @endif>{{ $category->title }}</category>
             @endforeach
         </categories>
         <offers>
             @foreach($products as $product)
                 <offer id="{{ $product->id }}" productId="{{ $product->id }}" quantity="9999">
                     <url>https://mossebo.market/ru/goods/{{ $product->id }}</url>
-                    <price>{{ $product->current_price->value / 100 }}</price>
-                    @foreach($product->categories as $product_cat)
-                        <categoryId>{{ $product_cat->id }}</categoryId>
-                    @endforeach
+                    @if ($product->relationNotEmpty('currentPrice'))
+                        <price>{{ $product->currentPrice->value }}</price>
+                    @endif
+                    @if ($product->relationNotEmpty('categoryRelations'))
+                        @foreach($product->categoryRelations as $categoryRelation)
+                            <categoryId>{{ $categoryRelation->category_id }}</categoryId>
+                        @endforeach
+                    @endif
                     <picture>
                         @if($product->image)
-                            https://admin.mossebo.market{{ json_decode($product->image->pathes)->original }}
+                            {{ imagePath(json_decode($product->image->pathes)->original) }}
                         @endif
                     </picture>
-                    <name>{{ $product->current_i18n->title }}</name>
-                    <productName>{{ $product->current_i18n->title }}</productName>
-                    @foreach($product->attributes as $attribute)
-                        @foreach($product->attribute_options as $attribute_option)
-                            @if($attribute_option->attribute_id == $attribute->id)
-                                <param name="{{ $attribute->current_i18n->title }}" code="atrr{{$attribute->id}}">
-                                    {{ $attribute_option->current_i18n->value }}
-                                </param>
-                            @endif
+                    <name>{{ $product->title }}</name>
+                    <productName>{{ $product->title }}</productName>
+                    @if ($product->relationNotEmpty('attributes') && $product->relationNotEmpty('attribute_options'))
+                        @foreach($product->attributes as $attribute)
+                            @foreach($product->attribute_options as $attribute_option)
+                                @if($attribute_option->attribute_id == $attribute->id)
+                                    <param name="{{ $attribute->title }}" code="attr-{{$attribute->id}}">
+                                        {{ $attribute_option->value }}
+                                    </param>
+                                @endif
+                            @endforeach
                         @endforeach
-                    @endforeach
-                    <param name="Описание" code="description">{{ $product->current_i18n->description }}</param>
+                    @endif
+                    <param name="Описание" code="description">{{ $product->description }}</param>
                     <param name="Артикул" code="article">{{ $product->id }}</param>
                     <param name="Вес" code="weight">{{ $product->weight / 1000 }} кг</param>
                     <dimensions>{{ $product->length / 10 }}/{{ $product->width / 10 }}/{{ $product->height / 10 }}</dimensions>
