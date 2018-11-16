ad3250c6c0373f85b8495ddc6c28b6c0c98a5476
Yuri Bondar <trigur@yandex.ru>
2018-11-16 14:35:33 +0300

    fix

--- app/Http/Controllers/Shop/CheckoutController.php
+++ app/Http/Controllers/Shop/CheckoutController.php
@@ -35,7 +35,7 @@ class CheckoutController extends Controller
             $confirmationLink = $this->handleNewOrder($order);
         }
         catch(\Exception $e) {
-//            dd($e);
+            dd($e);
             return response([
                 'status'   => 'error',
                 'message' => trans('errors.default')
